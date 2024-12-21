4ee3f3725d15571281ca65fa4ca6d738fe863e7b
Kiran Pantha <info@kiranpantha.com.np>
2024-12-21 11:01:17 +0545

    Added Repository for Candidate

--- dotnettask/Repositories/CandidateRepository.cs
+++ dotnettask/Repositories/CandidateRepository.cs
@@ -2,7 +2,11 @@ namespace CandidateHubAPI.Repositories
 {
     public class CandidateRepository : ICandidateRepository
     {
+        private readonly string _connectionString;
 
+        public CandidateRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
     }
-
 }
